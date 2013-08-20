537089c9158113208c11c26805cd72138472e980
Ikey Doherty <ikey@solusos.com>
2013-08-20 19:19:56 +0100

    Force depmod for specific version in kernel package
    Summary:
    Previously the post-install of the comar package ran depmod without
    a forced version number. Without this, kernel upgrades that have different
    version numbers will not work with depmod. This prevents both dracut and
    grub from being able to take the steps required to enable a bootable
    system.
    
    Maniphest Tasks: T2
    
    Differential Revision: http://inf.solusos.com/D3

--- kernel/image/kernel/comar/package.py
+++ kernel/image/kernel/comar/package.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import os
 
+KernelVersion = "3.10.6"
+
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-	# Must run depmod to keep the modules up to date :)
-	os.system ("/sbin/depmod")
-	
+    # Must run depmod to keep the modules up to date :)
+    os.system("/sbin/depmod %s", KernelVersion)
