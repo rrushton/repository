f072366cb34053fd4213243f9e143b4b7c0daede
Ikey Doherty <ikey@solusos.com>
2013-08-20 19:24:24 +0100

    Fix postInstall operations, provide package handler for upgrades
    Summary:
    dracut needs to upgrade initrd any time a new kernel is installed,
    as opposed to post-install operation.
    Depends on D3
    
    Maniphest Tasks: T2
    
    Differential Revision: http://inf.solusos.com/D4

--- system/boot/dracut/comar/package.py
+++ system/boot/dracut/comar/package.py
@@ -3,13 +3,13 @@ import os
 import os.path
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-	# Generate an initramfs for all installed kernels
+    # Generate an initramfs for all installed kernels
     for possible in os.listdir ("/boot"):
         if possible.startswith ("kernel-") or possible.startswith ("vmlinuz-"):
             version = possible.split ("-")[1]
             cmd = "dracut -f --kver %s" % version
             os.system (cmd)
-	
+
     # Determine whether to actually update grub or not.
-    if os.path.exists ("/dev/root"):
+    if os.path.exists("/proc/cmdline"):
         os.system ("/usr/sbin/update-grub")
---
+++ system/boot/dracut/comar/packageHandler.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import piksemel
+import os
+import os.path
+
+
+def updateInitrd(filepath):
+    parse = piksemel.parse(filepath)
+    for xmlfile in parse.tags("File"):
+        path = xmlfile.getTagData("Path")
+        if "/boot/kernel-" in path or "/boot/vmlinuz-" in path:
+            version = path.split("-")[1]
+            cmd = "dracut -f --kver %s" % version
+            os.system(cmd)
+
+        # Determine whether to actually update grub or not.
+        if os.path.exists("/proc/cmdline"):
+            os.system("/usr/sbin/update-grub")
+
+
+def setupPackage(metapath, filepath):
+    updateInitrd(filepath)
+
+
+def postCleanupPackage(metapath, filepath):
+    updateInitrd(filepath)
