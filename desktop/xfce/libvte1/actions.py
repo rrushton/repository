79b25b3f06119dcc3e06d169e9a218c1d412f41c
Ikey Doherty <ikey@solusos.com>
2013-08-21 18:51:10 +0100

    Add libvte1 to repositories

---
+++ desktop/xfce/libvte1/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Created For SolusOS
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-static")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc ("AUTHORS", "ChangeLog", "COPYING")
