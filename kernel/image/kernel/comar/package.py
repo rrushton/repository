f55e0e9cef2864e7fa8cdfbdefb3894c3f029652
Ikey Doherty <ikey@solusos.com>
2013-08-24 17:19:41 +0100

    Fix comar script in kernel package

--- kernel/image/kernel/comar/package.py
+++ kernel/image/kernel/comar/package.py
@@ -5,4 +5,4 @@ KernelVersion = "3.10.6"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     # Must run depmod to keep the modules up to date :)
-    os.system("/sbin/depmod %s", KernelVersion)
+    os.system("/sbin/depmod %s" % KernelVersion)
