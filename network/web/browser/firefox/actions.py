ad5da225380da1bc7a1f86ac80af2f08cf430b29
Ikey Doherty <ikey@solusos.com>
2013-08-25 21:10:33 +0100

    Do What Must Be Done - Firefox from Mozilla.

--- network/web/browser/firefox/actions.py
+++ network/web/browser/firefox/actions.py
@@ -1,15 +1,9 @@
-
 #!/usr/bin/python
 
 # Created For SolusOS
 
-from pisi.actionsapi import autotools, shelltools, pisitools
+from pisi.actionsapi import pisitools
 
-def build():
-    autotools.make ("-f client.mk")
-    #autotools.make ("-C firefox-build-dir/browser/installer")
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodoc ("LICENSE", "AUTHORS", "COPYING", "ChangeLog", "CHANGELOG")
+    pisitools.insinto ("/opt/firefox", "*")
