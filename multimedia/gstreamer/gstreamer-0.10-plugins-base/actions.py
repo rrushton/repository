af8b8b677135061cf89ef0abc605249911585f4a
Ikey Doherty <ikey@solusos.com>
2013-08-25 23:00:46 +0100

    Add gstreamer-0.10-plugins-base-0.10.36 to repo

---
+++ multimedia/gstreamer/gstreamer-0.10-plugins-base/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Created For SolusOS
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+shelltools.export("HOME", get.workDIR())
+
+
+def setup():
+    autotools.configure("--disable-static")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
