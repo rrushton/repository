d6bc81fb8f0cf375c22949864d87d9b31a69db00
Ikey Doherty <ikey@solusos.com>
2013-09-03 17:08:47 +0100

    Add solusos-branding as part of solusos-artwork

--- desktop/theme/solusos-artwork/actions.py
+++ desktop/theme/solusos-artwork/actions.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 
 # Created For SolusOS
-
 from pisi.actionsapi import pisitools
 
-	
+ThemeDir = "solusos-solusos-branding-8f5ff16b690d/themes"  # v0.1
+
+
 def install():
-    pisitools.insinto ("/usr/share", "gnome-background-properties")
-    pisitools.insinto ("/usr/share", "backgrounds")
+    pisitools.insinto("/usr/share", "gnome-background-properties")
+    pisitools.insinto("/usr/share", "backgrounds")
 
-	
-	
-
+    pisitools.insinto("/usr/share/themes", "%s/SolusOS-Darker" % ThemeDir)
