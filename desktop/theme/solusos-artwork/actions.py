770e11df1d3b6efb4a3d86405f6dbc200e24dca2
Ikey Doherty <ikey@solusos.com>
2013-09-03 18:23:32 +0100

    Update solusos-branding to latest xfwm changes, etc.

--- desktop/theme/solusos-artwork/actions.py
+++ desktop/theme/solusos-artwork/actions.py
@@ -3,11 +3,12 @@
 # Created For SolusOS
 from pisi.actionsapi import pisitools
 
-ThemeDir = "solusos-solusos-branding-8f5ff16b690d/themes"  # v0.1
+ThemeDir = "solusos-solusos-branding-8e7c5dcc956e/"  # v0.2
 
 
 def install():
     pisitools.insinto("/usr/share", "gnome-background-properties")
     pisitools.insinto("/usr/share", "backgrounds")
 
-    pisitools.insinto("/usr/share/themes", "%s/SolusOS-Darker" % ThemeDir)
+    pisitools.insinto("/usr/share/themes", "%s/themes/SolusOS-Darker" % ThemeDir)
+    pisitools.insinto("/usr/share/icons", "%s/icons/SolusOS" % ThemeDir)
