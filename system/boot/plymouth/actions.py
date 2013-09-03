40eb4b19a45b9c995193fd3e33506ff231a5abed
Ikey Doherty <ikey@solusos.com>
2013-09-03 22:47:43 +0100

    Update to latest git master snapshot of plymouth

--- system/boot/plymouth/actions.py
+++ system/boot/plymouth/actions.py
@@ -9,11 +9,6 @@ def setup():
     shelltools.system("./autogen.sh")
     autotools.configure("--enable-pango \
                          --enable-gtk \
-                         --without-rhgb-compat-link \
-                         --enable-libdrm_intel \
-                         --enable-libdrm_radeon \
-                         --enable-libdrm_nouveau \
-                         --enable-libkms \
                          --enable-drm-renderer \
                          --enable-systemd-integration \
                          --with-logo=/usr/share/pixmaps/SolusOS_Splash.png")
