e4342af758dcc434cef441ceffe8b255be0225da
Ikey Doherty <ikey@solusos.com>
2013-09-05 16:36:41 +0100

    Add software-update-icon v0.1 to repo

---
+++ desktop/core/software-update-icon/actions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# Created For SolusOS
+
+from pisi.actionsapi import get, shelltools, pisitools
+
+
+def install():
+    pisitools.dobin("software-update-icon.py")
+    shelltools.move("%s/usr/bin/software-update-icon.py" % get.installDIR(), "%s/usr/bin/software-update-icon" % get.installDIR())
+    pisitools.insinto("/etc/xdg/autostart", "software-update-icon.desktop")
