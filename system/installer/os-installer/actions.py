682c02213147402ff030de9b13cd4dce8d2145a1
Ikey Doherty <ikey@solusos.com>
2013-09-08 20:19:00 +0100

    Various fixes to os-installer

--- system/installer/os-installer/actions.py
+++ system/installer/os-installer/actions.py
@@ -2,7 +2,7 @@
 
 # Created For SolusOS
 
-from pisi.actionsapi import pythonmodules, pisitools
+from pisi.actionsapi import pythonmodules, pisitools, shelltools, get
 
 
 def build():
@@ -20,18 +20,18 @@ def install():
 
     # Icons
     pisitools.insinto("/usr/share/icons", "dist/logo.png", "os-installer.png")
-    pisitools.insinto("/usr/share/icons/gnome/scalable/action", "dist/icons/*")
+    pisitools.insinto("/usr/share/icons/gnome/scalable/actions", "dist/icons/*")
 
     # Desktop file
     pisitools.insinto("/usr/share/applications", "dist/os-installer.desktop")
-    pisitools.dosym("/usr/share/applications/os-installer.desktop", \
-                    "/etc/xdg/autostart/os-installer.desktop")
-    pisitools.dosym("/usr/share/applications/os-installer.desktop", \
-                    "/etc/skel/Desktop/os-installer.desktop")
+    pisitools.insinto("/etc/skel/Desktop", "dist/os-installer.desktop")
+    shelltools.chmod("%s/etc/skel/Desktop/os-installer.desktop" % get.installDIR())
                     
     # Configuration file
     pisitools.insinto("/etc/os-installer", "dist/install.conf")
 
     # PolicyKit conf
     pisitools.insinto("/usr/share/polkit-1/actions", "dist/*.policy")
+
+    pisitools.dobin("dist/os-installer-wrapper")
 
