9fb2824ac8f53ec9476c36fde665e8f8d9bdcf4e
Ikey Doherty <ikey@solusos.com>
2013-09-11 14:00:47 +0100

    Update clang/llvm to 3.3

--- programming/llvm/actions.py
+++ programming/llvm/actions.py
@@ -8,14 +8,10 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 def setup():
 	# Move the clang stuff to be usable
-	shelltools.move ("tools/clang-3.2.src", "tools/clang")
-	shelltools.move ("projects/compiler-rt-3.2.src", "projects/compiler-rt")
+	shelltools.move ("tools/cfe-3.3.src", "tools/clang")
+	shelltools.move ("projects/compiler-rt-3.3.src", "projects/compiler-rt")
 	
-	shelltools.system ("sed -e \"s@../lib/libprofile_rt.a@../lib/llvm/libprofile_rt.a@g\" -i tools/clang/lib/Driver/Tools.cpp")
-	
-	# "Final" release has broken compiler-rt, attempts to include darwin specific stuff ..
-	shelltools.system ("patch -b projects/compiler-rt/make/platform/clang_darwin.mk < fix_darwin_issue.patch")
-	
+		
 	shelltools.export ("CC", "gcc")
 	shelltools.export ("CXX", "g++")
 	autotools.rawConfigure ("--prefix=/usr              \
@@ -39,5 +35,5 @@ def install():
 	shelltools.chmod ("%s/usr/lib/llvm/*.a" % get.installDIR(), mode=0644)
 	
 	# Fix the llvm link
-	pisitools.dosym ("/usr/lib/llvm/libLLVM-3.2.so", "/usr/lib/libLLVM-3.2.so")
+	pisitools.dosym ("/usr/lib/llvm/libLLVM-3.3.so", "/usr/lib/libLLVM-3.3.so")
 	
