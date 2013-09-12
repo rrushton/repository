1e3b6b878593cfabb230a2e95be0b239a1ed0aac
Ikey Doherty <ikey@solusos.com>
2013-09-12 09:47:58 +0100

    [UPDATE] PHP 5.5.3

--- programming/php/actions.py
+++ programming/php/actions.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 
 # Created For SolusOS
@@ -7,11 +6,41 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-	autotools.configure()
-						
+    autotools.configure("--with-config-file-path=/etc \
+                         --with-zlib                  \
+                         --with-curl                  \
+                         --enable-bcmath              \
+                         --with-bz2                   \
+                         --enable-calendar            \
+                         --disable-dba                \
+                         --with-gmp                   \
+                         --enable-ftp                 \
+                         --with-gettext               \
+                         --enable-mbstring            \
+                         --with-readline")
+
+
 def build():
-	autotools.make()
-	
+    autotools.make()
+
+
 def install():
-	autotools.install()
-	pisitools.dodoc("LICENSE", "CREDITS")
+    shelltools.export("INSTALL_ROOT", get.installDIR())
+    autotools.install()
+
+    # Now move it all.. absurd build because only parts of php
+    # respect INSTALL_ROOT ..
+    pisitools.domove("%s/usr/*" % get.installDIR(), "/usr")
+    pisitools.domove("/usr/lib/lib/*", "/usr/lib/.")
+    pisitools.domove("/usr/lib/php/php/*", "/usr/lib/php/.")
+    pisitools.removeDir("/usr/lib/php/php")
+    pisitools.removeDir("/usr/lib/lib")
+    pisitools.removeDir("/var")
+    pisitools.remove("/.*")
+    pisitools.removeDir("/.registry")
+    pisitools.removeDir("/.channels")
+    pisitools.remove("/usr/lib/php/.*")
+    pisitools.removeDir("/usr/lib/php/.channels")
+    pisitools.removeDir("/usr/lib/php/.registry")
+    pisitools.dodoc("LICENSE", "CREDITS")
+    pisitools.dosym("/usr/bin/php", "/usr/bin/php5")
