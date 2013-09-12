a2be082b2ba38230c6bfb91bb83520ca13cb789d
Ikey Doherty <ikey@solusos.com>
2013-09-12 14:11:46 +0100

    [UPDATE] Sudo 1.8.7
     - CVE-2013-1776
    
    sudo 1.3.5 through 1.7.10 and 1.8.0 through 1.8.5, when the tty_tickets option is enabled, does not properly validate the controlling terminal device, which allows local users with sudo permissions to hijack the authorization of another terminal via vectors related to connecting to a standard input, output, and error file descriptors of another terminal. NOTE: this is one of three closely-related vulnerabilities that were originally assigned CVE-2013-1776, but they have been SPLIT because of different affected versions.

--- system/base/sudo/actions.py
+++ system/base/sudo/actions.py
@@ -1,24 +1,21 @@
-
 #!/usr/bin/python
-
 # Created For SolusOS
 
-from pisi.actionsapi import shelltools, get, autotools, pisitools
+from pisi.actionsapi import get, autotools, pisitools
 
 
 def setup():
-    autotools.configure ("--libexecdir=/usr/lib/sudo \
-                          --docdir=/usr/share/doc/sudo \
-                          --with-all-insults \
-                          --with-env-editor ")
-						
-def build():
-	autotools.make ()
-	
-def install():
-	autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
-	
-	pisitools.dodoc ("ChangeLog")
-	
-	
+    autotools.configure("--libexecdir=/usr/lib/sudo \
+                        --docdir=/usr/share/doc/sudo \
+                        --with-all-insults \
+                        --with-env-editor ")
 
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("ChangeLog")
