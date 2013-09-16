e712d63f3ee95be6167c564a9afae9d561ecfa8b
Ikey Doherty <ikey@solusos.com>
2013-09-16 13:29:43 +0100

    [UPDATE] Update Weston to 1.2.2

--- desktop/wayland/weston/actions.py
+++ desktop/wayland/weston/actions.py
@@ -7,16 +7,15 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-	autotools.configure ("--disable-libunwind")
+    autotools.configure ("--disable-libunwind    \
+                          --enable-weston-launch \
+                          --enable-demo-clients")
 						
 def build():
 	autotools.make ()
 	
 def install():
     autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
-
-    for item in ["flower", "smoke", "image", "resizor", "eventdemo"]:
-        pisitools.dobin ("clients/%s" % item)
 
     pisitools.dodoc ("COPYING")
 
