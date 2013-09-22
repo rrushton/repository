8be1051f5825f2b4e89e2157cea7267276b13efe
Peter Passchier <peter@passchier.net>
2013-09-22 13:50:35 +0100

    Add xfce4-dev-tools to repositories
    Summary: Necessary to build xfce4-clipman-plugin
    
    Reviewers: ikey
    
    Differential Revision: http://inf.solusos.com/D27

---
+++ desktop/xfce/xfce4-dev-tools/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Created For SolusOS
+
+from pisi.actionsapi import autotools
+
+
+def setup():
+    autotools.configure()
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
