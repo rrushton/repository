d7a8c0196b4a9173bf7879f89546b459b8e22109
Ikey Doherty <ikey@solusos.com>
2013-10-01 13:57:51 +0100

    [NEW] Add libwebp to repo (for webkitgtk)

---
+++ multimedia/codecs/libwebp/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Created For SolusOS
+
+from pisi.actionsapi import autotools, pisitools, get
+
+def setup():
+    autotools.configure("--disable-static \
+                         --enable-libwebpmux \
+                         --enable-libwebpdemux \
+                         --enable-everything \
+                         --enable-libwebpdecoder")
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("COPYING")
