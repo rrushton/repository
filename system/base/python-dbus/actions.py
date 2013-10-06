d51dafa3b735c2211b95d187623a988d3161f796
Ikey Doherty <ikey@solusos.com>
2013-10-06 12:47:59 +0100

    Add python3 to python-dbus modules

--- system/base/python-dbus/actions.py
+++ system/base/python-dbus/actions.py
@@ -3,15 +3,34 @@
 
 # Created For SolusOS
 
-from pisi.actionsapi import shelltools, get, autotools, pisitools
+from pisi.actionsapi import shelltools, get, autotools, pisitools   
+
+py2dir = "py2build"
+py3dir = "py3build"
+
+def configure(pvers, flags):
+    shelltools.system("PYTHON=%s ../configure %s" % (pvers, flags))
 
 def setup():
-	autotools.configure ("--prefix=/usr\
-						  --docdir=/usr/share/doc/python-dbus")
+    confFlags = "--prefix=/usr \
+                 --docdir=/usr/share/doc/python-dbus"
+    shelltools.makedirs(py2dir)
+    shelltools.cd(py2dir)
+    configure ("/usr/bin/python", confFlags)
+    shelltools.cd("..")
+    shelltools.makedirs(py3dir)
+    shelltools.cd(py3dir)
+    configure("/usr/bin/python3", confFlags)
 
 def build():
-	autotools.make ()
+    shelltools.cd(py2dir)
+    autotools.make ()
+    shelltools.cd("..")
+    shelltools.cd(py3dir)
 	
 def install():
-	autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
-	
+    shelltools.cd(py2dir)
+    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    shelltools.cd("..")
+    shelltools.cd(py3dir)
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
