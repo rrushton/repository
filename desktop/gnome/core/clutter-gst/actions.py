32abdbdcc1841ff701317fdac464e2dc6a3d13d9
Ikey Doherty <ikey@solusos.com>
2013-10-06 20:20:21 +0100

    [NEW] Add clutter-gst to repos

---
+++ desktop/gnome/core/clutter-gst/actions.py
@@ -0,0 +1,23 @@
+
+#!/usr/bin/python
+
+# Created For SolusOS
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+shelltools.export("HOME", get.workDIR())
+
+
+def setup():
+    autotools.configure("--disable-static \
+                         --enable-introspection")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING")
