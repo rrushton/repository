b4810f82c60d51c2b760cb459e4ee69e91143c64
Ikey Doherty <ikey@solusos.com>
2013-10-06 22:12:52 +0100

    [NEW] Add grilo-plugins to repos

---
+++ desktop/gnome/core/grilo-plugins/actions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+# Created For SolusOS
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    # TODO: Totem for --enable-optical-media, --enable-youtube,
+    #                 --enable-vimeo
+    #       gssdp for --enable-upnp
+    #       gmime for --enable-podcasts 
+    autotools.configure("--enable-goa \
+                         --enable-filesystem \
+                         --enable-jamendo \
+                         --enable-lastm-albumart \
+                         --enable-flickr \
+                         --enable-bookmarks \
+                         --enable-shoutcast \
+                         --enable-apple-trailers \
+                         --enable-magnatune \
+                         --enable-gravatar \
+                         --enable-tracker")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
