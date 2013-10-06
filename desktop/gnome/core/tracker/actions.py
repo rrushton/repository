307f1f59fce5466dfb46346d2219b4a7409b0c94
Ikey Doherty <ikey@solusos.com>
2013-10-06 21:00:40 +0100

    [NEW] Add tracker to repos

---
+++ desktop/gnome/core/tracker/actions.py
@@ -0,0 +1,43 @@
+
+#!/usr/bin/python
+
+# Created For SolusOS
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+shelltools.export("HOME", get.workDIR())
+
+
+def setup():
+    # TODO: --enable-mp3, --enable-poppler --enable-playlist
+    autotools.configure("--disable-static \
+                         --enable-introspection \
+                         --enable-journal \
+                         --enable-gnome-keyring \
+                         --enable-libsecret \
+                         --enable-network-manager \
+                         --enable-libexif \
+                         --enable-exempi \
+                         --enable-miner-fs \
+                         --enable-miner-thunderbird \
+                         --enable-miner-firefox \
+                         --enable-nautilus-extension \
+                         --enable-gdkpixbuf \
+                         --enable-libgif \
+                         --enable-libjpeg \
+                         --enable-libtiff \
+                         --enable-libpng \
+                         --enable-libvorbis \
+                         --enable-libflac \
+                         --enable-text \
+                         --enable-icon")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
