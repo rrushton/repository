15f14a883422d1b460cda5310453d149139fd924
Ikey Doherty <ikey@solusos.com>
2013-10-06 20:35:42 +0100

    [NEW] Add libchamplain to repos

---
+++ desktop/gnome/core/libchamplain/actions.py
@@ -0,0 +1,24 @@
+
+#!/usr/bin/python
+
+# Created For SolusOS
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+shelltools.export("HOME", get.workDIR())
+
+
+def setup():
+    autotools.configure("--disable-static \
+                         --enable-vala \
+                         --enable-introspection")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
