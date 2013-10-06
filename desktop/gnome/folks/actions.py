4aef1f699e18b68a488fb30ea687fa382ca6dcac
Ikey Doherty <ikey@solusos.com>
2013-10-07 00:25:19 +0100

    [UPDATE] folks 0.9.5

--- desktop/gnome/folks/actions.py
+++ desktop/gnome/folks/actions.py
@@ -8,7 +8,11 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 shelltools.export ("HOME", get.workDIR())
 
 def setup():
-	autotools.configure ("--disable-static")
+	autotools.configure ("--disable-static \
+                          --disable-fatal-warnings \
+                          --enable-telepathy-backend \
+                          --enable-tracker-backend \
+                          --enable-import-tool")
 						
 def build():
 	autotools.make ()
