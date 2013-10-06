77eb1233bca85410c75e169e9b60702eb4b9cd19
Ikey Doherty <ikey@solusos.com>
2013-10-06 13:12:10 +0100

    [NEW] Add xdg-user-dirs to the repos
    Signed-off-by: Ikey Doherty <ikey@solusos.com>

---
+++ desktop/core/xdg-user-dirs/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Created For SolusOS
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    shelltools.export("CONF_SHELL", "/bin/bash")
+    autotools.configure("--disable-documentation")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
