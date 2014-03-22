c1a42fd0f0c769735c10d0d5b64b7d8d709150c5
Ikey Doherty <ikey.doherty@gmail.com>
2014-03-22 20:58:33 +0000

    gcc: Disable multiarch for now (no 32bit support)

--- system/devel/gcc/actions.py
+++ system/devel/gcc/actions.py
@@ -7,21 +7,8 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 BuildDir = "%s/%s" % ( get.workDIR(), "gcc-build")
 GccDir = "../gcc-4.8.1"
-
-def get_multiarch_host ():
-	if get.ARCH() == "x86_64":
-		return "x86_64-linux-gnu"
-	else:
-		return "i386-linux-gnu"
-		
-def exportMultiVars ():
-	# Export multiarch required variables
-	shelltools.export ("CPATH", "/usr/include/%s" % get_multiarch_host())
-	shelltools.export ("LIBRARY_PATH", "/usr/lib/%s" % get_multiarch_host())
 	
 def setup():
-	exportMultiVars ()
-
 	shelltools.makedirs (BuildDir)
 	
 	shelltools.cd (BuildDir)
@@ -41,26 +28,22 @@ def setup():
 						--enable-ld=default\
 						--with-plugin-ld=ld.gold\
 						--enable-clocale=gnu\
-						--enable-multiarch\
+						--disable-multiarch\
+						--disable-multilib\
 						--enable-lto\
-						--with-multiarch-defaults=i386-linux-gnu\
-						--with-arch-32=i686\
 						--with-bugurl='http://bugs.evolve-os.com'\
 						--build=%s\
-						--host=%s\
 						--target=%s\
-						--enable-languages=c,c++,fortran" % (GccDir, get.HOST(), get.HOST(), get.HOST()) )
+						--enable-languages=c,c++,fortran" % (GccDir, get.HOST(), get.HOST()) )
 						
 	# Print the summary
 	shelltools.system ("%s/contrib/test_summary" % GccDir)
 						
 def build():
-	exportMultiVars ()
 	shelltools.cd (BuildDir)
 	autotools.make ()
 		
 def install():
-	exportMultiVars ()
 	shelltools.cd (BuildDir)
 	
 	autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
