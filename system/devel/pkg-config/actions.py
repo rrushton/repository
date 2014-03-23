77b871451e7fbffd31c7b60a70b81535e1264879
Ikey Doherty <ikey.doherty@gmail.com>
2014-03-23 18:08:08 +0000

    pkgconfig: Drop multiarch voodoo

--- system/devel/pkg-config/actions.py
+++ system/devel/pkg-config/actions.py
@@ -7,24 +7,15 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-	# Multiarch tuples (be compatible with Ubuntu & Debian's multiarch implementation
-	if "i686" in get.HOST():
-		m_tuple = "i386-linux-gnu"
-	else:
-		m_tuple = " x86_64-linux-gnu"
-		
-	pcDirs = "/usr/local/lib/%(MULTIARCH)s/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig:/usr/lib/%(MULTIARCH)s/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig" % { 'MULTIARCH' : m_tuple }
-	autotools.configure("--prefix=/usr \
-						 --docdir=/usr/share/doc/pkg-config \
-						 --with-internal-glib \
-						 --with-pc-path=%s\
-						 --disable-host-tool" % pcDirs)
+    autotools.configure("--prefix=/usr \
+                         --docdir=/usr/share/doc/pkg-config \
+                         --with-internal-glib")
 					
 def build():
-	autotools.make()
+    autotools.make()
 	
 def install():
-	autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-	
-	pisitools.dodoc ("AUTHORS", "COPYING", "ChangeLog", "README")
-	
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc ("AUTHORS", "COPYING", "ChangeLog", "README")
+
