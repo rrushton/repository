eac81554252ff83d5555b0778978b7a6696e0bba
Ikey Doherty <ikey.doherty@gmail.com>
2014-03-23 16:54:05 +0000

    baselayout: Update for Evolve OS changes

--- system/base/baselayout/actions.py
+++ system/base/baselayout/actions.py
@@ -13,25 +13,24 @@ def install():
     # Install everything
     pisitools.insinto("/", "root/*")
 
-    for dire in ["/tmp", "/var/tmp", "/dev", "/proc", "/sys", "/var/lock", "/root", "/home"]:
+    for dire in ["/tmp", "/var/tmp", "/dev", "/proc", "/sys", "/run/lock", "/root", "/home", "/run"]:
 		pisitools.dodir (dire)
 		
     # Adjust permissions
     do_chmod("/tmp", 01777)
     do_chmod("/var/tmp", 01777)
-    do_chmod("/var/lock", 0775)
+    do_chmod("/run/lock", 0775)
     do_chmod("/usr/share/baselayout/shadow", 0600)
     do_chmod("/dev", 0755)
     do_chmod("/proc", 0555)
     do_chmod("/sys", 0755)
     do_chmod("/home", 0755)
+    do_chmod("/run", 0755)
+    pisitools.dosym("/run", "/var/run")
+    pisitools.dosym("/run/lock", "/var/lock")
 
     pisitools.dosym ("/proc/self/mounts", "/etc/mtab")
 
     # Write out a default .profile.. temporary
     shelltools.echo ("%s/etc/skel/.profile" % get.installDIR(), "source /etc/profile")
     shelltools.echo ("%s/etc/skel/.bashrc" % get.installDIR(), "source /etc/profile")
-
-    # Bump this ourselves in baselayout next time
-    pisitools.dosed ("%s/etc/issue" % get.installDIR(), "Alpha8", "Alpha 9")
-    pisitools.dosed ("%s/usr/share/baselayout/issue" % get.installDIR(), "Alpha8", "Alpha 9")
