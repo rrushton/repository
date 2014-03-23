989f378bf75445ddbc86699f2f7adde77d03dfa1
Ikey Doherty <ikey.doherty@gmail.com>
2014-03-23 17:11:13 +0000

    comar: Drop flags and use correct WorkDir

--- system/base/comar/actions.py
+++ system/base/comar/actions.py
@@ -8,6 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+WorkDir = "comar-3.0.3/comar"
+
 def setup():
     cmaketools.configure("-DPYTHON_INCLUDE_DIR=/usr/include/python2.7 \
                           -DPYTHON_LIBRARY=/usr/lib/libpython2.7.so")
