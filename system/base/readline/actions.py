e483e5adcb6b4ce855a6d34dbd88f5c9c002b316
Ikey Doherty <ikey.doherty@gmail.com>
2014-03-23 18:55:25 +0000

    readline: Experimental change, fix initial bootstrap

--- system/base/readline/actions.py
+++ system/base/readline/actions.py
@@ -7,12 +7,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-def get_multiarch_host ():
-	if get.ARCH() == "x86_64":
-		return "x86_64-linux-gnu"
-	else:
-		return "i386-linux-gnu"
-		
 def setup():
     autotools.configure("--with-curses --disable-static"  )
 def build():
@@ -22,6 +16,6 @@ def install():
     autotools.rawInstall("DESTDIR=%s install" % get.installDIR())
 
     pisitools.removeDir("/usr/bin")
-
+    pisitools.dosym("../usr/lib/libreadline.so.6.2", "/lib64/libreadline.so.2")
     pisitools.dohtml("doc/")
     pisitools.dodoc("CHANGELOG", "CHANGES", "README", "USAGE", "NEWS")
