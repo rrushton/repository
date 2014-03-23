594920e6acd8a7436133c9b88ec955cf5197e4b9
Ikey Doherty <ikey.doherty@gmail.com>
2014-03-23 15:33:59 +0000

    openssl: Fix shadow and perl-docs conflicts

--- system/base/openssl/actions.py
+++ system/base/openssl/actions.py
@@ -19,3 +19,7 @@ def install():
 	autotools.rawInstall("INSTALL_PREFIX=%s MANDIR=/usr/share/man" % get.installDIR())
 	# Move pkgconfig back into /usr/lib/pkgconfig
 	pisitools.domove("/usr/lib64/pkgconfig", "/usr/lib")
+
+	# openssl decided it needed more conflicts. and more man pages.
+	pisitools.rename("/usr/share/man/man1/passwd.1", "passwd.openssl.1") # shadow conflict
+	pisitools.rename("/usr/share/man/man3/threads.3", "threads.openssl.3") # perl-docs conflict
