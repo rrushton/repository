264f973d5d25a3475ff46659570ab915ebefde4a
Ikey Doherty <ikey.doherty@gmail.com>
2014-04-30 10:26:20 +0100

    grub: Disable -fPIC

--- system/boot/grub/actions.py
+++ system/boot/grub/actions.py
@@ -8,6 +8,7 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 def setup():
 	shelltools.system ("sed -i -e '/gets is a/d' grub-core/gnulib/stdio.in.h")
 	cflags = get.CFLAGS().replace("-fstack-protector","") # Fix build issue.
+	cflags = cflags.replace("-fPIC", "") # clobbered asm issue (x86)
 	shelltools.export ("CFLAGS", cflags)
 	autotools.configure ("--prefix=/usr\
 						  --sysconfdir=/etc\
