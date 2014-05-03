96b91c0e5df3b0a3a74fafb6e7ab41f9e68c2a25
Ikey Doherty <ikey.doherty@gmail.com>
2014-05-03 20:43:37 +0100

    binutils: Fix ld for glibc, and full-native builds failing
    glibc essentially throws a large bitch-fit when we try to use ld.gold,
    despite various mechanisms being available to suggest using the standard
    ld. It then proceeds to just use "/usr/bin/ld" and ignore you anyway.
    
    So, now we use ld, not ld.gold. Also there was a slight issue with some
    builds due to the native ld being missing, this commit rectifies that.

--- system/devel/binutils/actions.py
+++ system/devel/binutils/actions.py
@@ -9,25 +9,27 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 BuildDir = "%s/%s" % ( get.workDIR(), "binutils-build")
 
 def setup():
-	# Don't install outdated standards.info
-	shelltools.unlink ("etc/standards.info")
-	shelltools.system ("sed -i.bak '/^INFO/s/standards.info //' etc/Makefile.in")
-	shelltools.makedirs (BuildDir)
-	
-	shelltools.cd (BuildDir)
-	# Configure
-	shelltools.system ("../binutils-2.23.2/configure --prefix=/usr --enable-lto --enable-gold --target=x86_64-evolveos-linux")
-	
-def build():
-	shelltools.cd (BuildDir)
-	autotools.make ("tooldir=/usr")
-		
-def install():
-	shelltools.cd (BuildDir)
-	autotools.rawInstall ("tooldir=/usr DESTDIR=%s" % get.installDIR())
-	
-	# Include the libiberty header
-	pisitools.insinto ("/usr/include", "../binutils-2.23.2/include/libiberty.h")
+    # Don't install outdated standards.info
+    shelltools.unlink ("etc/standards.info")
+    shelltools.system ("sed -i.bak '/^INFO/s/standards.info //' etc/Makefile.in")
+    shelltools.makedirs (BuildDir)
 
-	# Use gold by default
-	pisitools.dosym("/usr/bin/x86_64-evolveos-linux-ld.gold", "/usr/bin/ld")
+    shelltools.cd (BuildDir)
+    # Configure
+    shelltools.system ("../binutils-2.23.2/configure \
+        --prefix=/usr \
+        --enable-lto \
+        --enable-gold \
+        --target=x86_64-evolveos-linux \
+        --build=x86_64-evolveos-linux")
+
+def build():
+    shelltools.cd (BuildDir)
+    autotools.make ("tooldir=/usr")
+
+def install():
+    shelltools.cd (BuildDir)
+    autotools.rawInstall ("tooldir=/usr DESTDIR=%s" % get.installDIR())
+
+    # Include the libiberty header
+    pisitools.insinto ("/usr/include", "../binutils-2.23.2/include/libiberty.h")
