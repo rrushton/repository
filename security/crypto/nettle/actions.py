a1aa36d92030e042fa8efa2c5d7c7540a450e50f
Ikey Doherty <ikey.doherty@gmail.com>
2014-05-05 12:15:04 +0100

    nettle: Force libdir to /usr/lib (temporary)

--- security/crypto/nettle/actions.py
+++ security/crypto/nettle/actions.py
@@ -7,15 +7,15 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.configure ()
+    autotools.configure("--libdir=/usr/lib")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     for target in ["/usr/lib/libhogweed.so.2.3", "/usr/lib/libnettle.so.4.5"]:
-        shelltools.chmod ("%s/%s" % (get.installDIR(), target), mode=0755)
+        shelltools.chmod("%s/%s" % (get.installDIR(), target), mode=0755)
 
-    pisitools.dodoc ("nettle.html")
+    pisitools.dodoc("nettle.html")
