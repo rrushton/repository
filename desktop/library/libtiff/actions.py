403a340d158afa225fbd03b32e51e4895d0bc3b3
Ikey Doherty <ikey.doherty@gmail.com>
2014-06-08 12:54:29 +0100

    libtiff: Fix splitting, use pkgconfig+auto-deps

--- desktop/library/libtiff/actions.py
+++ desktop/library/libtiff/actions.py
@@ -8,8 +8,8 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 def setup():
     autotools.configure("--prefix=/usr\
-                                             --disable-jbig\
-                                             --disable-static")
+                         --disable-jbig\
+                         --disable-static")
 
 def build():
     autotools.make()
