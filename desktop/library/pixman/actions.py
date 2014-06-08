d8cb60079c0aa9d43727603f38fc1c50b6938178
Ikey Doherty <ikey.doherty@gmail.com>
2014-06-08 11:21:23 +0100

    pixman: Dep fixes, splitting, disable static build (see pspec.xml)

--- desktop/library/pixman/actions.py
+++ desktop/library/pixman/actions.py
@@ -7,7 +7,7 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
