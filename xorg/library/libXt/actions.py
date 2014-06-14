f7d209e097d2ce518df1a25751ef01350ed6c5a4
Ikey Doherty <ikey.doherty@gmail.com>
2014-06-14 18:18:34 +0100

    libxt: Fix splitting, use pkgconfig+auto-deps

--- xorg/library/libXt/actions.py
+++ xorg/library/libXt/actions.py
@@ -7,10 +7,11 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.configure ("--with-appdefaultdir=/etc/X11/app-defaults")
+    autotools.configure("--with-appdefaultdir=/etc/X11/app-defaults \
+                         --disable-static")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.install ()
+    autotools.install()
