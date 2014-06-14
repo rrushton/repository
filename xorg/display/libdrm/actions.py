7c0061d0fe1b0040696d4216e8ffcb79a5831ac7
Ikey Doherty <ikey.doherty@gmail.com>
2014-06-14 15:33:36 +0100

    libdrm: Fix splitting, use pkgconfig+auto-deps

--- xorg/display/libdrm/actions.py
+++ xorg/display/libdrm/actions.py
@@ -8,14 +8,14 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 def setup():
     # Disable linking to thread-stubs
-    shelltools.system ("sed -e \"/pthread-stubs/d\" -i configure.ac")
+    shelltools.system("sed -e \"/pthread-stubs/d\" -i configure.ac")
 
-    autotools.autoreconf ("-fi")
-    autotools.configure ("--disable-static --enable-udev --prefix=/usr")
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static --enable-udev --prefix=/usr")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc ("README")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("README")
