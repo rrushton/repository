2f37a09f039c890848150d4920316fa31f558cf5
Ikey Doherty <ikey.doherty@gmail.com>
2014-06-15 12:59:07 +0100

    gstreamer-1.0: Fix splitting, use pkgconfig(implicit)+auto-deps

--- multimedia/gstreamer/gstreamer-1.0/actions.py
+++ multimedia/gstreamer/gstreamer-1.0/actions.py
@@ -5,18 +5,18 @@
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 
-shelltools.export ("HOME", get.workDIR())
+shelltools.export("HOME", get.workDIR())
 
 def setup():
-    autotools.configure ("--disable-static \
-                                              --libexecdir=/usr/lib \
-                                              --with-package-name=\"GStreamer 1.0.6 SolusOS\" \
-                                               --with-package-origin=http://www.solusos.com")
+    autotools.configure("--disable-static \
+                         --libexecdir=/usr/lib \
+                         --with-package-name=\"GStreamer 1.0.6 Evolve OS\" \
+                         --with-package-origin=http://www.evolve-os.com")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc ("AUTHORS", "ChangeLog", "COPYING")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
