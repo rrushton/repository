c57e0649c34e90661a52deac051017686d63e825
Ikey Doherty <ikey.doherty@gmail.com>
2014-06-15 19:11:35 +0100

    font-droid-ttf: Fix upstream tarball location

--- desktop/font/font-droid-ttf/actions.py
+++ desktop/font/font-droid-ttf/actions.py
@@ -6,13 +6,13 @@
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 import glob
 
-WorkDir = "."
+WorkDir = "ttf-droid-1.00~b112+dfsg+1"
 
 FontDir = "/usr/share/fonts/truetype/droid/"
 
 def install():
 
-    for i in glob.glob ("*.ttf"):
-        pisitools.insinto (FontDir, i)
+    for i in glob.glob("*.ttf"):
+        pisitools.insinto(FontDir, i)
 
-        pisitools.dodoc ("README")
+        pisitools.dodoc("README")
