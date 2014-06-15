7fd1ba1a3421a0ee382262654f3fe0c031cc0b98
Ikey Doherty <ikey.doherty@gmail.com>
2014-06-15 15:40:39 +0100

    gnome-desktop: Fix splitting, use pkgconfig+auto-deps

--- desktop/gnome/core/gnome-desktop/actions.py
+++ desktop/gnome/core/gnome-desktop/actions.py
@@ -5,17 +5,17 @@
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 
-shelltools.export ("HOME", get.workDIR())
+shelltools.export("HOME", get.workDIR())
 
 def setup():
-    autotools.configure ("--disable-static \
-                          --libexecdir=/usr/lib/GnomeDesktop \
-                          --with-gnome-distributor=SolusOS")
+    autotools.configure("--disable-static \
+                         --libexecdir=/usr/lib/GnomeDesktop \
+                         --with-gnome-distributor=\"Evolve OS\"")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc ("AUTHORS", "ChangeLog", "COPYING")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
