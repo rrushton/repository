e3e0c557e547b3bc0f3200ea87079a0e25d8af9b
Ikey Doherty <ikey.doherty@gmail.com>
2014-06-21 12:31:08 +0100

    cups: Fix splitting, use auto-deps

--- desktop/core/cups/actions.py
+++ desktop/core/cups/actions.py
@@ -7,24 +7,23 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.aclocal ("-I config-scripts")
-    #autotools.autoreconf ("-I config-scripts -vfi")
-    autotools.configure ("--libdir=/usr/lib \
-                          --without-rcdir \
-                          --with-docdir=/usr/share/cups/doc \
-                          --with-system-groups=lpadmin \
-                          --enable-openssl \
-                          --enable-acl \
-                          --enable-dbus \
-                          --enable-libpaper ")
+    autotools.aclocal("-I config-scripts")
+    autotools.configure("--libdir=/usr/lib \
+                         --without-rcdir \
+                         --with-docdir=/usr/share/cups/doc \
+                         --with-system-groups=lpadmin \
+                         --enable-openssl \
+                         --enable-acl \
+                         --enable-dbus \
+                         --enable-libpaper ")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("BUILDROOT=%s" % get.installDIR() )
+    autotools.rawInstall("BUILDROOT=%s" % get.installDIR() )
 
-    pisitools.removeDir ("/usr/share/cups/banners")
-    pisitools.removeDir ("/usr/share/cups/data/testprint")
+    pisitools.removeDir("/usr/share/cups/banners")
+    pisitools.removeDir("/usr/share/cups/data/testprint")
 
-    shelltools.echo ("%s/etc/cups/client.conf" % get.installDIR(), "ServerName /var/run/cups/cups.sock")
+    shelltools.echo("%s/etc/cups/client.conf" % get.installDIR(), "ServerName /var/run/cups/cups.sock")
