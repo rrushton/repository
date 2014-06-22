dca6e946b7fcaefbe83ec074d0e683e8850d432f
Ikey Doherty <ikey.doherty@gmail.com>
2014-06-22 16:53:25 +0100

    webkitgtk+: Fix splitting, use pkgconfig+auto-deps

--- desktop/web/webkitgtk+/actions.py
+++ desktop/web/webkitgtk+/actions.py
@@ -5,31 +5,31 @@
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 
-shelltools.export ("HOME", get.workDIR())
+shelltools.export("HOME", get.workDIR())
 
-def build_config ():
-    cflags = get.CFLAGS().replace ("-ggdb3","")
-    cxxflags = get.CXXFLAGS().replace ("-gddb3", "")
-    shelltools.export ("CFLAGS", cflags)
-    shelltools.export ("CXXFLAGS", cxxflags)
+def build_config():
+    cflags = get.CFLAGS().replace("-ggdb3","")
+    cxxflags = get.CXXFLAGS().replace("-gddb3", "")
+    shelltools.export("CFLAGS", cflags)
+    shelltools.export("CXXFLAGS", cxxflags)
 
 
 def setup():
-    shelltools.system ("sed -i '/gtkdoc --rebase/s:^:# :' GNUmakefile.*")
-    if not get.canClang(): build_config ()
-    autotools.configure ("--disable-static \
-                          --libexecdir=/usr/lib/WebKitGTK \
-                          --with-gstreamer=1.0 \
-                          --with-gnu-ld \
-                          --enable-introspection \
-                          --disable-geolocation \
-                          --disable-gtk-doc-html")
-    shelltools.system ("sed -i '/gtkdoc --rebase/s:^:# :' GNUmakefile.*")
+    shelltools.system("sed -i '/gtkdoc --rebase/s:^:# :' GNUmakefile.*")
+    if not get.canClang(): build_config()
+    autotools.configure("--disable-static \
+                         --libexecdir=/usr/lib/WebKitGTK \
+                         --with-gstreamer=1.0 \
+                         --with-gnu-ld \
+                         --enable-introspection \
+                         --disable-geolocation \
+                         --disable-gtk-doc-html")
+    shelltools.system("sed -i '/gtkdoc --rebase/s:^:# :' GNUmakefile.*")
 
 def build():
-    if not get.canClang(): build_config ()
-    autotools.make ()
+    if not get.canClang(): build_config()
+    autotools.make()
 
 def install():
-    if not get.canClang(): build_config ()
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    if not get.canClang(): build_config()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
