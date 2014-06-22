ce70fc0df2d40da5ce25f8b0266c9555ab710514
Ikey Doherty <ikey.doherty@gmail.com>
2014-06-22 16:15:56 +0100

    ruby: Fix splitting

--- programming/ruby/actions.py
+++ programming/ruby/actions.py
@@ -8,9 +8,9 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 def setup():
     autotools.autoconf()
     autotools.configure("--disable-rpath \
-                                             --enable-shared \
-                                             --enable-pthread\
-                                             --with-sitedir=/usr/lib/ruby/site_ruby")
+                         --enable-shared \
+                         --enable-pthread\
+                         --with-sitedir=/usr/lib/ruby/site_ruby")
 
 def build():
     autotools.make()
