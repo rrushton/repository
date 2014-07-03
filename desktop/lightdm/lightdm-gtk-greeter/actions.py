6f6c9f62ced3e51c7f4d8b9847e488e03d5193b7
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-03 19:48:33 +0100

    lightdm-gtk-greeter: Use pkgconfig+auto-deps, revert branding

--- desktop/lightdm/lightdm-gtk-greeter/actions.py
+++ desktop/lightdm/lightdm-gtk-greeter/actions.py
@@ -7,12 +7,12 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.configure ()
+    autotools.configure()
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.install ()
+    autotools.install()
 
-    pisitools.dodoc ("AUTHORS", "ChangeLog", "COPYING")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
