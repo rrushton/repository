73d3b69b4f70a0a8235b607b9d669014ef747637
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-04 00:20:03 +0100

    mozjs17: Fix splitting

--- desktop/library/mozjs17/actions.py
+++ desktop/library/mozjs17/actions.py
@@ -9,15 +9,15 @@ import os
 BuildDir = "js/src"
 
 def setup():
-    os.chdir (BuildDir)
-    autotools.configure ("--prefix=/usr --enable-threadsafe --with-system-nspr")
+    os.chdir(BuildDir)
+    autotools.configure("--prefix=/usr --enable-threadsafe --with-system-nspr")
 
 def build():
-    os.chdir (BuildDir)
-    autotools.make ()
+    os.chdir(BuildDir)
+    autotools.make()
 
 def install():
-    os.chdir (BuildDir)
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    os.chdir(BuildDir)
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    shelltools.system ("sed -i %s/usr/lib/pkgconfig/mozjs-17.0.pc -e 's@/var/pisi/mozjs.*/install@@g'" % get.installDIR())
+    shelltools.system("sed -i %s/usr/lib/pkgconfig/mozjs-17.0.pc -e 's@/var/pisi/mozjs.*/install@@g'" % get.installDIR())
