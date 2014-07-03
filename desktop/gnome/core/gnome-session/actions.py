93440fa00e09a529654ea6c8da9a4114a2a7d490
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-03 19:05:11 +0100

    gnome-session: Use pkgconfig+auto-deps

--- desktop/gnome/core/gnome-session/actions.py
+++ desktop/gnome/core/gnome-session/actions.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 
 # Created For SolusOS
@@ -7,15 +6,14 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.configure ("--disable-static \
-                          --libexecdir=/usr/lib/gnome-session \
-                          --enable-systemd \
-                          --disable-docbook-docs")
+    autotools.configure("--disable-static \
+                         --libexecdir=/usr/lib/gnome-session \
+                         --enable-systemd")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc ("AUTHORS", "ChangeLog", "COPYING")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
