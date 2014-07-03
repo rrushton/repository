65eee3ac0462969ad28a3f4ca3ac3ff20a68c67c
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-03 23:42:11 +0100

    gconf: Fix splitting, use auto-deps

--- desktop/gnome/core/gconf/actions.py
+++ desktop/gnome/core/gconf/actions.py
@@ -5,21 +5,21 @@
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 
-shelltools.export ("HOME", get.workDIR())
+shelltools.export("HOME", get.workDIR())
 
 def setup():
-    autotools.configure ("--prefix=/usr \
-                                              --sysconfdir=/etc \
-                                              --libexecdir=/usr/lib/GConf \
-                                              --disable-orbit \
-                                              --disable-static")
+    autotools.configure("--prefix=/usr \
+                         --sysconfdir=/etc \
+                         --libexecdir=/usr/lib/GConf \
+                         --disable-orbit \
+                         --disable-static")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dosym ("/etc/gconf/gconf.xml.defaults", "/etc/gconf/gconf.xml.system")
+    pisitools.dosym("/etc/gconf/gconf.xml.defaults", "/etc/gconf/gconf.xml.system")
 
-    pisitools.dodoc ("ChangeLog", "COPYING")
+    pisitools.dodoc("ChangeLog", "COPYING")
