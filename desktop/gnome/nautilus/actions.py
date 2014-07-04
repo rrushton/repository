5cc4d338493814bf434b516613d75cf4569b592e
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-04 23:09:44 +0100

    nautilus: Fix splitting, use pkgconfig+auto-deps

--- desktop/gnome/nautilus/actions.py
+++ desktop/gnome/nautilus/actions.py
@@ -1,28 +1,27 @@
-
 #!/usr/bin/python
 
 # Created For SolusOS
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 
-shelltools.export ("HOME", get.workDIR())
+shelltools.export("HOME", get.workDIR())
 
 def setup():
-    autotools.configure ("--disable-tracker \
-                          --disable-static \
-                          --disable-packagekit \
-                          --libexecdir=/usr/lib/nautilus")
+    autotools.configure("--disable-tracker \
+                         --disable-static \
+                         --enable-packagekit \
+                         --libexecdir=/usr/lib/nautilus")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc ("COPYING")
+    pisitools.dodoc("COPYING")
 
     # Cleanup
     for item in ["XMLnamespaces", "mime.cache", "icons", "aliases", \
                  "types", "magic", "generic-icons", "subclasses", "globs", \
                  "version", "globs2", "treemagic"]:
-        pisitools.remove ("/usr/share/mime/%s" % item)
+        pisitools.remove("/usr/share/mime/%s" % item)
