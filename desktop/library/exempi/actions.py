4d1aecbd6bdc3b41c48b93d2f7429f7794138b35
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-04 22:38:45 +0100

    exempi: Fix splitting

--- desktop/library/exempi/actions.py
+++ desktop/library/exempi/actions.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 
 # Created For SolusOS
@@ -7,12 +6,12 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.configure ("--disable-static")
+    autotools.configure("--disable-static")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR() )
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR() )
 
-    pisitools.dodoc ("AUTHORS", "ChangeLog", "COPYING")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
