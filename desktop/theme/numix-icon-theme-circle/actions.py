d24866b7d2f8d692b10901d8b620d4ad0c01c17e
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-04 17:47:39 +0100

    numix-icon-theme-circle: Ensure icons are readable

--- desktop/theme/numix-icon-theme-circle/actions.py
+++ desktop/theme/numix-icon-theme-circle/actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-from pisi.actionsapi import pisitools
+from pisi.actionsapi import pisitools, shelltools, get
 
 def install():
     pisitools.insinto("/usr/share/icons/Numix-Circle", "Numix-Circle/*")
+    shelltools.system("chmod a+r -R %s/usr/share/icons/Numix-Circle" % get.installDIR())
