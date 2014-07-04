ab5ba78d7c6e9dc6ec9b554d40e53bf5c9a18e76
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-04 17:49:31 +0100

    xorg-driver-video-intel: Use pkgconfig+auto-deps

--- xorg/driver/xorg-driver-video-intel/actions.py
+++ xorg/driver/xorg-driver-video-intel/actions.py
@@ -7,13 +7,13 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.configure ("--enable-kms-only \
+    autotools.configure("--enable-kms-only \
                           --with-default-accel=sna")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc ("COPYING")
+    pisitools.dodoc("COPYING")
