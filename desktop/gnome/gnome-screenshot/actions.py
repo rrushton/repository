92f37327f500b393e58ba13c43d236764fdcb955
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-05 11:36:33 +0100

    gnome-screenshot: Add version 3.10.1 to repos

---
+++ desktop/gnome/gnome-screenshot/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Created For Evolve OS
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+
+
+def setup():
+    autotools.configure()
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("COPYING")
