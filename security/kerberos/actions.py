af92dee6d38869ee8e01944edd49c26bb5aac7b0
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-05 20:09:31 +0100

    kerberos: Fix splitting

--- security/kerberos/actions.py
+++ security/kerberos/actions.py
@@ -13,23 +13,23 @@ BuildDir = "%s/krb5-1.11/src" % get.workDIR()
 
 def setup():
     print "Extracting source"
-    os.system ("tar xf %s" % SourceDist)
+    os.system("tar xf %s" % SourceDist)
 
-    os.chdir (BuildDir)
+    os.chdir(BuildDir)
     autotools.configure("--enable-dns-for-realm")
 
 def build():
-    os.chdir (BuildDir)
+    os.chdir(BuildDir)
     autotools.make("-j1")
 
 def install():
-    os.chdir (BuildDir)
+    os.chdir(BuildDir)
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     # Fix library permissions
     for library in ["gssapi_krb5", "gssrpc", "k5crypto", "kadm5clnt_mit", \
                     "kdb5", "krb5", "krb5support", "verto"]:
-        shelltools.chmod ("%s/usr/lib/lib%s.so*" % (get.installDIR(), library), mode=0755)
+        shelltools.chmod("%s/usr/lib/lib%s.so*" %(get.installDIR(), library), mode=0755)
 
     # This conflicts with e2fsprogs
-    pisitools.remove ("/usr/lib/libcom_err.so")
+    pisitools.remove("/usr/lib/libcom_err.so")
