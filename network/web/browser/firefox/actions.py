ca13a07abeac35cce4d302febcdaa9516a3659bf
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-05 15:49:39 +0100

    firefox: Fix actions.py(missing bracket)

--- network/web/browser/firefox/actions.py
+++ network/web/browser/firefox/actions.py
@@ -15,7 +15,7 @@ def install():
     autotools.rawInstall("-f client.mk install INSTALL_SDK= DESTDIR=%s" % get.installDIR())
     pisitools.dosym("/usr/lib/firefox-30.0/browser/icons/mozicon128.png",
                      "/usr/share/pixmaps/firefox.png")
-    pisitools.dodir("/usr/lib/mozilla/plugins"
+    pisitools.dodir("/usr/lib/mozilla/plugins")
     pisitools.dosym("/usr/lib/mozilla/plugins",
                     "/usr/lib/firefox-30.0/.")
 
