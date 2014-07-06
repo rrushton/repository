cbeb68d019f914be34679df99909535029942251
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-06 18:17:56 +0100

    xorg-driver-video-vmware: Use pkgconfig+auto-deps

--- xorg/driver/xorg-driver-video-vmware/actions.py
+++ xorg/driver/xorg-driver-video-vmware/actions.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 
 # Created For SolusOS
@@ -7,12 +6,13 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.configure ()
+    autotools.configure()
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
-    pisitools.removeDir ("/usr/bin")
-    pisitools.dodoc ("COPYING")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    # Stoopid.
+    pisitools.removeDir("/usr/bin")
+    pisitools.dodoc("COPYING")
