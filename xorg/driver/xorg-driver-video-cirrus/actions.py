8c7057c11ad0dc3e239d9075ac784849528fc292
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-06 17:49:48 +0100

    xorg-driver-video-cirrus: Use pkgconfig+auto-deps

--- xorg/driver/xorg-driver-video-cirrus/actions.py
+++ xorg/driver/xorg-driver-video-cirrus/actions.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 
 # Created For SolusOS
@@ -7,12 +6,12 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.configure ()
+    autotools.configure()
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.install ()
+    autotools.install()
 
-    pisitools.dodoc ("ChangeLog", "COPYING")
+    pisitools.dodoc("ChangeLog", "COPYING")
