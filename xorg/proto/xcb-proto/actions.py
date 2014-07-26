1e9638735d799b01115bd8faa52bed27562e9b06
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-26 14:16:43 +0100

    xcb-proto: Update to latest upstream version

--- xorg/proto/xcb-proto/actions.py
+++ xorg/proto/xcb-proto/actions.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 
 # Created For SolusOS
@@ -7,11 +6,10 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.configure ("--disable-static")
+    autotools.configure("--disable-static")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
-    #pisitools.removeDir ("/usr/share/")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
