6d52c0de0b5b0c2eeefc5ce651f671fce3aac4df
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-26 16:13:26 +0100

    glamor-egl: Update to latest upstream version

--- xorg/display/glamor-egl/actions.py
+++ xorg/display/glamor-egl/actions.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python
 
-# Created For SolusOS
-
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.autoreconf("-fi")
     autotools.configure("--enable-glx-tls")
 
 def build():
