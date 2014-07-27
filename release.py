bc6452f5b110acea27166598d5ec9938f2212ced
Ikey Doherty <ikey.doherty@gmail.com>
2014-07-27 14:09:05 +0100

    Update release.py for Evolve OS

--- release.py
+++ release.py
@@ -3,33 +3,23 @@
 #
 #  release.py - Handle releases
 #  
-#  Copyright 2013 Ikey Doherty <ikey@solusos>
+#  Copyright 2014 Ikey Doherty <ikey.doherty@gmail.com>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
+#
+
 import subprocess
 import time
 from datetime import date
 import sys
 
-MAJOR = "2"
+MAJOR = "0"
 
 release_who = "Ikey Doherty"
-release_mail = "ikey@solusos.com"
+release_mail = "ikey.doherty@gmail.com"
 should_filter = False
 
 
