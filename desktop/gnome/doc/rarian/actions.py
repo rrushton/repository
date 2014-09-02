50c969ad320b081388b8018017cdd2a6c0260dbc
Ikey Doherty <ikey.doherty@gmail.com>
2014-09-02 18:17:35 +0100

    [FIX] rarian: Clean up for Evolve OS

--- desktop/gnome/doc/rarian/actions.py
+++ desktop/gnome/doc/rarian/actions.py
@@ -1,19 +1,15 @@
-
 #!/usr/bin/python
 
-# Created For SolusOS
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 def setup():
     autotools.configure ("--prefix=/usr \
-                                              --localstatedir=/var \
-                                              --disable-static")
+                          --localstatedir=/var \
+                          --disable-static")
 
 def build():
     autotools.make ()
 
 def install():
     autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodoc ("NEWS", "COPYING", "COPYING.LIB", "AUTHORS")
