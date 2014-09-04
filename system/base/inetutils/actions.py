0f2ba888df036ad454c6cd7cd95462c21694cc66
Ikey Doherty <ikey.doherty@gmail.com>
2014-09-04 21:59:43 +0100

    [NEW] Add inetutils to base system

---
+++ system/base/inetutils/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Created For Evolve OS
+
+from pisi.actionsapi import get, autotools, pisitools
+
+
+def setup():
+    # We'll use alternatives to logger/whois, and screw having telnetd or talkd.
+    autotools.configure("--disable-logger \
+                         --disable-whois \
+                         --disable-servers")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.removeDir("/usr/libexec")
