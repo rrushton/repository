b88c85eb70d67fd490495b216baa99c7a39a6fcd
Ikey Doherty <ikey.doherty@gmail.com>
2014-09-04 21:33:51 +0100

    [UPDATE] bash: Update to 4.2.047

--- system/base/bash/actions.py
+++ system/base/bash/actions.py
@@ -1,17 +1,15 @@
-
 #!/usr/bin/python
 
-# Created For SolusOS
 
-from pisi.actionsapi import shelltools, get, autotools, pisitools
+from pisi.actionsapi import get, autotools, pisitools
 
 
 def setup():
-    autotools.configure ("--prefix=/usr\
-                          --mandir=/usr/share/man\
-                          --infodir=/usr/share/info\
-                          --with-curses\
-                          --enable-history\
+    autotools.configure ("--prefix=/usr \
+                          --mandir=/usr/share/man \
+                          --infodir=/usr/share/info \
+                          --with-curses \
+                          --enable-history \
                           --with-installed-readline")
 
 def build():
