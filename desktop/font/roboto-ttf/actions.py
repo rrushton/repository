7f1158f3c7ba99fdaea5be91323e4baeb007bb0a
Ikey Doherty <ikey.doherty@gmail.com>
2014-09-07 15:11:20 +0100

    [NEW] font-droid-ttf: Add to repo for visual refresh

---
+++ desktop/font/roboto-ttf/actions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+
+from pisi.actionsapi import shelltools, get, autotools, pisitools
+import glob
+
+FontDir = "/usr/share/fonts/truetype/roboto/"
+WorkDir = "."
+
+def install():
+    for i in glob.glob("*.ttf"):
+        pisitools.insinto(FontDir, i)
