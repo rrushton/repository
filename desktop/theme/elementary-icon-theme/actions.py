06a16d33c2c58e4b348c054350c2caad7c822dd3
Ikey Doherty <ikey.doherty@gmail.com>
2014-09-07 16:41:20 +0100

    [UPDATE] elementary-icon-theme: Update to 3.1

--- desktop/theme/elementary-icon-theme/actions.py
+++ desktop/theme/elementary-icon-theme/actions.py
@@ -1,10 +1,6 @@
-
 #!/usr/bin/python
-
-# Created for solusos
 
 from pisi.actionsapi import pisitools
 
 def install():
-    pisitools.insinto("/usr/share/icons","elementary")
-    pisitools.insinto("/usr/share/icons",'elementary-mono-dark')
+    pisitools.insinto("/usr/share/icons/elementary","*")
