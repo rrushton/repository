4e05d8ac34f88fb283fddc2c3e92ee4b4f5cba77
Ikey Doherty <ikey.doherty@gmail.com>
2014-09-07 15:18:08 +0100

    [FIX] gtk2-engine-murrine: Cleanup packaging

--- desktop/theme/gtk2-engine-murrine/actions.py
+++ desktop/theme/gtk2-engine-murrine/actions.py
@@ -1,18 +1,15 @@
-
 #!/usr/bin/python
-
-# Created For SolusOS
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
 def setup():
-    autotools.configure ("--disable-static")
+    autotools.configure("--disable-static")
 
 def build():
-    autotools.make ()
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc ("ChangeLog", "COPYING")
+    pisitools.dodoc("ChangeLog", "COPYING")
