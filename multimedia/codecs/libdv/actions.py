228fe12f9eca2ec74876ca63af6282d3d5b0bacd
Ikey Doherty <ikey.doherty@gmail.com>
2014-09-08 20:40:47 +0100

    [NEW] libdv: Add for GStreamer enabling

---
+++ multimedia/codecs/libdv/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Created For Evolve OS
+
+from pisi.actionsapi import get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-static \
+                         --disable-xv")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("COPYING", "ChangeLog", "AUTHORS")
