b1fd62f7ccbb71b53de512dabf5de498bf05d1ba
Ikey Doherty <ikey@evolve-os.com>
2014-09-13 16:26:10 +0100

    [NEW] libass: Add for GStreamer enabling

---
+++ multimedia/library/libass/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Created For Evolve OS
+
+from pisi.actionsapi import get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-static")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
