568e6aac3960a09df3c64787f00ce1265375bb9e
Ikey Doherty <ikey@evolve-os.com>
2014-09-18 20:35:22 +0100

    [UPDATE] pidgin: Update to latest upstream version

--- network/im/pidgin/actions.py
+++ network/im/pidgin/actions.py
@@ -1,21 +1,16 @@
-
 #!/usr/bin/python
 
-# Created For SolusOS
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 def setup():
-    '''
-    Notes: Pidgin will require GStreamer 0.10 series,
-    Also a GTK2 gtkspell
-    '''
     autotools.configure("--disable-vv \
                          --disable-idn \
                          --disable-meanwhile \
                          --disable-gtkspell \
                          --disable-gstreamer \
                          --disable-schemas-install \
+                         --disable-tcl \
                          --disable-avahi")
 
 def build():
