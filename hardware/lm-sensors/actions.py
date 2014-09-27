4760270fb1c903cbd89599f661c41b456ddc5a58
Ikey Doherty <ikey@evolve-os.com>
2014-09-27 19:58:47 +0100

    [NEW] lm_sensors: Add for enabling

---
+++ hardware/lm-sensors/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Created For Evolve OS
+
+from pisi.actionsapi import get, autotools, pisitools
+
+opts = "PREFIX=/usr MANDIR=/usr/share/man BUILD_STATIC_LIB=0"
+
+def build():
+    autotools.make(opts)
+
+
+def install():
+    autotools.rawInstall("%s DESTDIR=%s" % (opts, get.installDIR()))
+
+    
