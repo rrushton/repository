7368f7d4491f56977571cc997454dd932aa30af5
Ikey Doherty <ikey@evolve-os.com>
2014-10-17 19:44:22 +0100

    [UPDATE] libgoffice: Update to 0.10.18

--- office/libgoffice/actions.py
+++ office/libgoffice/actions.py
@@ -1,19 +1,19 @@
-
 #!/usr/bin/python
 
-# Created For SolusOS
+# Created For Evolve OS
 
-from pisi.actionsapi import shelltools, get, autotools, pisitools
+from pisi.actionsapi import get, autotools, pisitools
 
-shelltools.export ("HOME", get.workDIR())
 
 def setup():
-    autotools.configure ("--disable-static")
+    autotools.configure()
+
 
 def build():
-    autotools.make ()
+    autotools.make()
+
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc ("COPYING", "ChangeLog", "AUTHORS", "BUGS")
+    pisitools.dodoc("ChangeLog", "AUTHORS", "COPYING")
