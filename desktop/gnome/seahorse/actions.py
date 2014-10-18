84f828c2c89e7604ead1d85771bc1ed90adb87a0
Ikey Doherty <ikey@evolve-os.com>
2014-10-18 17:28:43 +0100

    [NEW] seahorse: Add to enable repo signing

---
+++ desktop/gnome/seahorse/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Created For Evolve OS
+
+from pisi.actionsapi import get, autotools, pisitools
+
+
+def setup():
+    autotools.configure("--disable-valac")
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    
