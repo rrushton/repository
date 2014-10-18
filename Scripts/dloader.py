2bc32bd33846fc8fed16663c76b2cc5f3da0f3b9
Ikey Doherty <ikey@evolve-os.com>
2014-10-18 19:03:46 +0100

    Scripts: Fallback when Content-Length is unknown

--- Scripts/dloader.py
+++ Scripts/dloader.py
@@ -2,28 +2,35 @@ import urllib2
 import hashlib
 
 def download_file(url):
-        file_name = url.split('/')[-1]
-        output = file_name
-        u = urllib2.urlopen(url)
-        f = open(output, 'wb')
-        meta = u.info()
-        file_size = int(meta.getheaders("Content-Length")[0])
+    file_name = url.split('/')[-1]
+    output = file_name
+    u = urllib2.urlopen(url)
+    f = open(output, 'wb')
+    meta = u.info()
 
-        file_size_dl = 0
-        block_sz = 8192
-        while True:
-                buffer = u.read(block_sz)
-                if not buffer:
-                        break
+    headers = meta.getheaders("Content-Length")
+    file_size = 0
+    if len(headers) >= 1:
+        file_size = int(headers[0])
 
-                file_size_dl += len(buffer)
-                f.write(buffer)
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+            buffer = u.read(block_sz)
+            if not buffer:
+                    break
+
+            file_size_dl += len(buffer)
+            f.write(buffer)
+            if file_size > 0:
                 status = r" [%3.2f%%] %s/%s %s" % (file_size_dl * 100. / file_size, sizeof_fmt(file_size_dl), sizeof_fmt(file_size), file_name)
-                status = status + chr(8)*(len(status)+1)
-                print status,
+            else:
+                status = r"  %s/unknown_size %s" % (sizeof_fmt(file_size_dl), file_name)
+            status = status + chr(8)*(len(status)+1)
+            print status,
 
-        print "\n"
-        f.close()
+    print "\n"
+    f.close()
 
 def sizeof_fmt(num):
         for x in ['bytes','KB','MB','GB']:
