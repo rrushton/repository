b8dd08c6d6be4335643063a539c7b03514fd37e8
Justin Zobel <justin.zobel@gmail.com>
2014-10-18 15:54:59 +1030

    [NEW] gnome-common: Add to repos
    ---
    Cleaned up spec
    
    Signed-off-by: Ikey Doherty <ikey@evolve-os.com>

---
+++ desktop/gnome/core/gnome-common/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Created For Evolve OS
+
+from pisi.actionsapi import get, autotools, pisitools
+
+
+def setup():
+    autotools.configure()
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("ChangeLog", "COPYING")
