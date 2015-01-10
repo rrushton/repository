52c128459b60bbd8f89c1cccc2f659f33ac379ee
Ikey Doherty <ikey@evolve-os.com>
2015-01-10 10:58:23 +0000

    [NEW] quirky: Add for people to play with.

---
+++ desktop/budgie/quirky/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Created For Evolve OS
+
+from pisi.actionsapi import get, autotools, pisitools
+
+
+def setup():
+    autotools.configure()
+
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.remove("/usr/bin/dummy-bot")
+
+    
