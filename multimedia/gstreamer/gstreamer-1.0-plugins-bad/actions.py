025615c3382cae224805335bad83803508073aab
Ikey Doherty <ikey@evolve-os.com>
2015-01-23 18:45:25 +0000

    gstreamer-1.0-plugins-bad: Update to 1.4.5

--- multimedia/gstreamer/gstreamer-1.0-plugins-bad/actions.py
+++ multimedia/gstreamer/gstreamer-1.0-plugins-bad/actions.py
@@ -9,7 +9,7 @@ shelltools.export("HOME", get.workDIR())
 
 def setup():
     autotools.configure("--disable-static \
-                         --with-package-name=\"GStreamer Base Plugins 1.4.1 Evolve OS\" \
+                         --with-package-name=\"GStreamer Bad Plugins 1.4.5 Evolve OS\" \
                          --with-package-origin=\"https://evolve-os.com\"")
 
 
