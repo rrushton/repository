4397eed2f20b394b67eb2f11a4e42585ea530039
Ikey Doherty <ikey@evolve-os.com>
2015-01-26 18:10:27 +0000

    faba-icon-theme: Fix file permissions due to using zip source

--- desktop/theme/faba-icon-theme/actions.py
+++ desktop/theme/faba-icon-theme/actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-from pisi.actionsapi import pisitools
+from pisi.actionsapi import pisitools, shelltools, get
 
 def install():
     pisitools.insinto("/usr/share/icons/Faba","*")
+    shelltools.system("chmod a+r -R %s/usr/share/icons/Faba" % get.installDIR())
