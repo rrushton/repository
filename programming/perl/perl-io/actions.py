d50af1a8c2ed56d94a856d9753cff94438eb6df2
Ikey Doherty <ikey@evolve-os.com>
2015-01-31 01:38:35 +0000

    perl-io: Add 1.25

---
+++ programming/perl/perl-io/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Created For Evolve OS
+
+from pisi.actionsapi import perlmodules, pisitools
+
+
+def setup():
+    perlmodules.configure()
+
+
+def build():
+    perlmodules.make()
+
+
+def install():
+    perlmodules.install()
+
+    pisitools.dodoc("ChangeLog")
