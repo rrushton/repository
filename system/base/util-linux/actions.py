172ede5c2340c751c438a31f3d4b143e87ef377e
Ikey Doherty <ikey@evolve-os.com>
2015-03-07 22:32:22 +0000

    util-linux: Update to 2.26

--- system/base/util-linux/actions.py
+++ system/base/util-linux/actions.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 
-# Created For SolusOS
-
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 # Circular deps  - woo
 IgnoreAutodep = True
 
 def setup():
-    shelltools.system("./autogen.sh")
     autotools.configure("--disable-login \
-                         --disable-su")
+                         --disable-su \
+                         --disable-static")
 
 def build():
     autotools.make()
@@ -20,5 +18,5 @@ def install():
     # Conflicts with e2fsprogs
     pisitools.remove("/usr/share/man/man8/fsck.8")
 
-    pisitools.dosym ("/bin/mount", "/usr/bin/mount")
-    pisitools.dosym ("/bin/umount", "/usr/bin/umount")
+    pisitools.dosym("/bin/mount", "/usr/bin/mount")
+    pisitools.dosym("/bin/umount", "/usr/bin/umount")
