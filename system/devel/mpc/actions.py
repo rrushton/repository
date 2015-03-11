84f038e2f09ca863cd0fbd25c8b52031621b3c17
Ikey Doherty <ikey@evolve-os.com>
2015-03-11 14:52:23 +0000

    mpc: Also go to system.base

--- system/devel/mpc/actions.py
+++ system/devel/mpc/actions.py
@@ -1,22 +1,13 @@
-
 #!/usr/bin/python
-
-# Created For SolusOS
-
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 
-
 def setup():
-    autotools.configure ("--prefix=/usr")
+    autotools.configure("--disable-static")
 
 def build():
-    autotools.make ()
-
-def check():
-    autotools.make ("check")
+    autotools.make()
 
 def install():
-    autotools.rawInstall ("DESTDIR=%s" % get.installDIR())
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    # TODO: Add docs
