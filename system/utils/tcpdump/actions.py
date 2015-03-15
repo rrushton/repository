477f2cfb7cb09ba64f8ed6303d92348c024babbd
Ikey Doherty <ikey@evolve-os.com>
2015-03-15 15:43:17 +0000

    tcpdump: Update to 4.7.3 to address multiple CVES
    This update addresses CVE-2014-8767 and CVE-2014-8769

--- system/utils/tcpdump/actions.py
+++ system/utils/tcpdump/actions.py
@@ -1,7 +1,4 @@
-
 #!/usr/bin/python
-
-# Created For SolusOS
 
 from pisi.actionsapi import shelltools, get, autotools, pisitools
 
@@ -9,10 +6,11 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 def setup():
     autotools.configure()
 
+
 def build():
     autotools.make()
 
+
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.remove("/usr/sbin/tcpdump.4.3.0")
-    pisitools.dodoc("CREDITS", "LICENSE", "README")
+    pisitools.dodoc("CREDITS", "LICENSE")
