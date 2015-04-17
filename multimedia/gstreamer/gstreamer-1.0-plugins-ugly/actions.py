36976dbaeffae7bd9e33df670e546a1c508bf27c
Ikey Doherty <ikey@solus-project.com>
2015-04-17 18:01:12 +0100

    gstreamer-1.0-plugins-ugly: Fix brand string

--- multimedia/gstreamer/gstreamer-1.0-plugins-ugly/actions.py
+++ multimedia/gstreamer/gstreamer-1.0-plugins-ugly/actions.py
@@ -7,8 +7,8 @@ from pisi.actionsapi import get, autotools, pisitools
 
 def setup():
     autotools.configure("--disable-static \
-                         --with-package-name=\"GStreamer Ugly Plugins 1.4.5 Evolve OS\" \
-                         --with-package-origin=\"https://evolve-os.com\"")
+                         --with-package-name=\"GStreamer Ugly Plugins 1.4.5 Solus\" \
+                         --with-package-origin=\"https://solus-project.com\"")
 
 
 def build():
