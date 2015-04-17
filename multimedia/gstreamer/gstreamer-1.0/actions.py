7bd422f8b9c069d54dd615492c2b0bf74d09aa10
Ikey Doherty <ikey@solus-project.com>
2015-04-17 18:08:42 +0100

    gstreamer-1.0: Finalize GST brand fixing

--- multimedia/gstreamer/gstreamer-1.0/actions.py
+++ multimedia/gstreamer/gstreamer-1.0/actions.py
@@ -7,8 +7,8 @@ shelltools.export("HOME", get.workDIR())
 def setup():
     autotools.configure("--disable-static \
                          --libexecdir=/usr/lib \
-                         --with-package-name=\"GStreamer 1.4.5 Evolve OS\" \
-                         --with-package-origin=https://evolve-os.com")
+                         --with-package-name=\"GStreamer 1.4.5 Solus\" \
+                         --with-package-origin=https://solus-project.com")
 
 def build():
     autotools.make()
