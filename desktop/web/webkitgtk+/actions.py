5d99ae391a3e5fa97aaea6acf9b7aa406aceb995
Ikey Doherty <ikey@solus-project.com>
2015-04-17 01:20:43 +0100

    webkitgtk: In-tree

--- desktop/web/webkitgtk+/actions.py
+++ desktop/web/webkitgtk+/actions.py
@@ -5,14 +5,10 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools, cmaketools
 shelltools.export("HOME", get.workDIR())
 
 def setup():
-    shelltools.makedirs("build-web")
-    shelltools.cd("build-web")
     cmaketools.configure("-DPORT=GTK")
 
 def build():
-    shelltools.cd("build-web")
     autotools.make()
 
 def install():
-    shelltools.cd("build-web")
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
