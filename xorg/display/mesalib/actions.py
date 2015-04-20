59e0b20b1e1b21c1878b8e0d3102e9e8f97f6204
Ikey Doherty <ikey@solus-project.com>
2015-04-20 05:54:35 +0100

    mesalib: Update to 10.5.3

--- xorg/display/mesalib/actions.py
+++ xorg/display/mesalib/actions.py
@@ -8,8 +8,6 @@ from pisi.actionsapi import shelltools, get, autotools, pisitools
 
 def setup():
     autotools.autoreconf ("-fi")
-    shelltools.export ("CFLAGS", "-O2")
-    shelltools.export ("CXXFLAGS", "-O2")
 
     #disabled r300,r600,radeonsi
     autotools.configure ("--prefix=/usr                  \
@@ -17,7 +15,6 @@ def setup():
                           --enable-texture-float         \
                           --enable-gles1                 \
                           --enable-gles2                 \
-                          --enable-openvg                \
                           --enable-osmesa                \
                           --enable-xa                    \
                           --enable-gbm                   \
