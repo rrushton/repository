984e4c7db18dd06ee5f749b4a95fe65922b86d95
Justin Zobel <justin.zobel@gmail.com>
2015-05-06 15:30:08 +0930

    numix-icon-theme: update to 0.3.369 and yml

--- desktop/theme/numix-icon-theme/actions.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from pisi.actionsapi import shelltools, get, autotools, pisitools
-
-def install():
-    pisitools.insinto("/usr/share/icons/Numix", "Numix/*")
-    shelltools.system("chmod a+r -R %s/usr/share/icons/Numix" % get.installDIR())
