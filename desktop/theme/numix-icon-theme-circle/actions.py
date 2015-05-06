81c6a83df22b2136b5e0f732937900b780c5ac2c
Justin Zobel <justin.zobel@gmail.com>
2015-05-06 15:15:25 +0930

    numix-circle: update to 0.2.20150207.1452 and yml

--- desktop/theme/numix-icon-theme-circle/actions.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from pisi.actionsapi import pisitools, shelltools, get
-
-def install():
-    pisitools.insinto("/usr/share/icons/Numix-Circle", "Numix-Circle/*")
-    shelltools.system("chmod a+r -R %s/usr/share/icons/Numix-Circle" % get.installDIR())
