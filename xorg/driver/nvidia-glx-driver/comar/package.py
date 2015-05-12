8a8715255133ffe930a40141129e793152d4f234
Ikey Doherty <ikey@solus-project.com>
2015-05-12 21:43:58 +0100

    nvidia-glx-driver: Add 346.59 to repos

---
+++ xorg/driver/nvidia-glx-driver/comar/package.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+import os.path
+
+def postInstall(fromVersion, fromRelease, toVersion, toRelease):
+    try:
+        os.system("/usr/bin/gl-driver-switch set-link nvidia")
+        os.system("nvidia-xconfig")
+    except Exception, e:
+        print "Post-install error: %s" % e
+
+def postRemove():
+    try:
+        os.system("/usr/bin/gl-driver-switch set-link default")
+        if os.path.exists("/etc/X11/xorg.conf"):
+            os.unlink("/etc/X11/xorg.conf")
+    except Exception, e:
+        print "Post-remove error: %s" % e
