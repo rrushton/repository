65e81dfe876dfe06554d68964fcb35cc03a4440d
Ikey Doherty <ikey@solus-project.com>
2015-05-16 14:58:10 +0100

    kernel: Update to 4.0.3

--- kernel/image/kernel/actions.py
+++ kernel/image/kernel/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-KVERSION = "4.0.2"
+KVERSION = "4.0.3"
 
 from pisi.actionsapi import kerneltools
 from pisi.actionsapi import shelltools
--- kernel/image/kernel/comar/package.py
+++ kernel/image/kernel/comar/package.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 
-KernelVersion = "4.0.2"
+KernelVersion = "4.0.3"
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     # Must run depmod to keep the modules up to date :)
