4962f5b47de3dcab0415a6e7635a92db2f3c1bfc
Justin Zobel <justin.zobel@gmail.com>
2015-05-21 13:15:21 +0930

    faba-icon-theme: update to 4.0 and yml

--- desktop/theme/faba-icon-theme/actions.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from pisi.actionsapi import pisitools, shelltools, get
-
-def install():
-    pisitools.insinto("/usr/share/icons/Faba","*")
-    shelltools.system("chmod a+r -R %s/usr/share/icons/Faba" % get.installDIR())
